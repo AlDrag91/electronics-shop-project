05603dcda7590d85f7a2e2963603bc2e9f8019e5
AlDrag91 <147440379+AlDrag91@users.noreply.github.com>
2023-11-19 17:56:41 +0400

    develop 1 homework electronics shop

--- src/item.py
+++ src/item.py
@@ -13,7 +13,9 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +23,14 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        total_price = self.price * self.pay_rate
+        price = total_price
+        return price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price - (self.price * self.quantity / 100)
+
+        self.all.extend([self.name, self.price, self.quantity])
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+
+def test_item():
+    item1 = Item("Камера", 10000, 25)
+    Item.pay_rate = 0.8
+    assert item1.calculate_total_price() == 8000
+    item1.apply_discount()
+    assert item1.price == 7500
+    assert Item.all == ["Камера", 7500, 25]
